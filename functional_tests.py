8244e061f2f476cea67fc977ef04fd88272b3d5f
Anton Masyuk <a.masyuk@comagic.dev>
2024-07-02 10:26:04 +0500

    add app with broken test

--- functional_tests.py
+++ functional_tests.py
@@ -11,12 +11,12 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get("http://localhost:8000")
 
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn("To-Do", self.browser.title)
 
         self.fail("Закончить тест!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
