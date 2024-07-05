0159aeca7cb22e42e73fa569d1864abd999175a2
Anton Masyuk <a.masyuk@comagic.dev>
2024-07-05 11:01:34 +0500

    Добавлены тесты для представления.

--- functional_tests.py
+++ functional_tests.py
@@ -1,22 +1,63 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        self.browser = webdriver.Chrome(
+            options=chrome_options,
+        )
 
     def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Эдит слышала про крутое новое онлайн-приложение со списком
+        # неотложных дел. Она решает оценить его домашнюю страницу
         self.browser.get("http://localhost:8000")
 
+        # Она видит, что заголовок и шапка страницы говорят о списках
+        # неотложных дел
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-DO", header_text)
+
+        # Ей сразу же предлагается ввести элемент списка
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(
+            input_box.get_attribute("placeholder"),
+            "Enter a to-do item",
+        )
+
+        # Она набирает в текстовом поле "Купить павлиньи перья"
+        # (ее хобби – вязание рыболовных мушек)
+        input_box.send_keys('Купить павлиньи перья')
+
+        # Когда она нажимает enter, страница обновляется, и теперь страница
+        # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == "1: Купить павлиньи перья" for row in rows))
+
+        # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
+        # Она вводит "Сделать мушку из павлиньих перьев"
+        # (Эдит очень методична)
+
 
         self.fail("Закончить тест!")
 
+        # Страница снова обновляется и теперь показывает оба элемента
+        # ее списка
 
 if __name__ == "__main__":
     unittest.main()
