1b50fdda765e91c998e4fce3a8d89b184b679124
Anton Masyuk <a.masyuk@comagic.dev>
2024-07-07 22:22:58 +0500

    Новый URL + представление для добавления в существующие списки. ФТ проходит :-)

--- lists/views.py
+++ lists/views.py
@@ -9,13 +9,13 @@ def home_page(request):
     return render(request, "home.html")
 
 
-def view_list(request):
+def view_list(request, list_id):
     """Представление списка."""
-    items = Item.objects.all()
+    list_ = List.objects.get(pk=list_id)
     return render(
         request,
         "list.html",
-        {"items": items},
+        {"list": list_},
     )
 
 
@@ -23,4 +23,10 @@ def new_list(request):
     """Новый список."""
     list_ = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=list_)
-    return redirect("/lists/the-one-list-in-the-world/")
+    return redirect(f"/lists/{list_.id}/")
+
+def add_item(request, list_id):
+    """Добавить элемент."""
+    list_ = List.objects.get(pk=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list_)
+    return redirect(f"/lists/{list_.id}/")
