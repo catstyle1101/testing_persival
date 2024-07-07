93da04f78e7752df18e4216dd4647a974fa49896
Anton Masyuk <a.masyuk@comagic.dev>
2024-07-07 22:26:43 +0500

    Рефакторинг urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from lists import views
 
 urlpatterns = [
     path("", views.home_page, name="home"),
-    path("lists/new", views.new_list, name="new_list"),
-    path("lists/<int:list_id>/", views.view_list, name="view_list"),
-    path("lists/<int:list_id>/add_item", views.add_item, name="add_item"),
+    path("lists/", include("lists.urls")),
 ]
