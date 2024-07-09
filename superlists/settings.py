4ccca6e5e814e2ebf955406ce8d5138f13a4fcd9
Anton Masyuk <pantera_box@mail.ru>
2024-07-09 21:53:45 +0500

    origins

--- superlists/settings.py
+++ superlists/settings.py
@@ -123,3 +123,7 @@ STATIC_ROOT = BASE_DIR.parent / "static"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://catstyle.ddns.net",
+]
