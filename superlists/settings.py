54c91cf1bda538d5bdaa1f083b1b3ce48f9adf78
Anton Masyuk <a.masyuk@comagic.dev>
2024-07-10 18:03:05 +0500

    debug settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-nl+_qrwe11ey7w%@(l)k5&^2-olzq((y5=3%plae5frg4-*jv1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") in ["True", 1, "1", "true"]
 
 ALLOWED_HOSTS = [
     "http://catstyle.ddns.net",
