ea32b0777432c87b9c7566dd0900c6ee8e98afbb
Anton Masyuk <pantera_box@mail.ru>
2024-07-11 18:14:20 +0500

    Перемещены ФТ в свои собственные отдельные файлы

---
+++ functional_tests/base.py
@@ -0,0 +1,39 @@
+import os
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common import WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+MAX_WAIT = 2
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+    def setUp(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        self.browser = webdriver.Chrome(
+            options=chrome_options,
+        )
+        staging_server = os.getenv("STAGING_SERVER")
+        if staging_server:
+            self.live_server_url = f"http://{staging_server}"
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        """Wait for the table row to appear."""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
