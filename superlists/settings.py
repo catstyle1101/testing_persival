e1be0c5f21febaf0e182c0f37610bbcfc90e2246
Anton Masyuk <pantera_box@mail.ru>
2024-08-10 15:29:38 +0500

    base.html + новое приложение

--- superlists/settings.py
+++ superlists/settings.py
@@ -131,3 +131,20 @@ CSRF_TRUSTED_ORIGINS = [
     "http://catstyle.ddns.net",
 ]
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
