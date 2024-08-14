76cf277d7b7fe9e2c723b85f8da4226d6c729c96
Anton Masyuk <pantera_box@mail.ru>
2024-08-14 17:46:30 +0500

    URL, представление-заготовка и примерные шаблоны для my_lists

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from selenium.webdriver.common.by import By
 
 from functional_tests.base import FunctionalTest
 from functional_tests.management.commands.create_session import \
@@ -27,11 +28,48 @@ class MyListsTest(FunctionalTest):
         )
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        email = "edith@example.com"
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_out(email)
+        # Эдит является зарегистрированным пользователем.
+        self.create_authenticated_session("edith@example.com")
 
-        # Эдит является зарегистрированным пользователем
-        self.create_authenticated_session(email)
+        # Эдит открывает домашнюю страницу и начинает новый список
         self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_in(email)
+        self.add_list_item("Reticulate splines")
+        self.add_list_item("Immanentize eschaton")
+        first_list_url = self.browser.current_url
+
+        # Она замечает ссылку на "мои списки" в первый раз.
+        self.browser.find_element(By.LINK_TEXT, "My lists").click()
+
+        # Она видит, что ее список находится там и он назван на основе первого
+        # элемента списка
+        self.wait_for(
+            lambda: self.browser.find_element(By.LINK_TEXT, "Reticulate splines")
+        )
+        self.browser.find_element(By.LINK_TEXT, "Reticulate splines").click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, first_list_url)
+        )
+
+        # Она решает начать еще один список, чтобы только убедиться
+        self.browser.get(self.live_server_url)
+        self.add_list_item("Click cows")
+        second_list_url = self.browser.current_url
+
+        # Под заголовком "мои списки" появляется ее новый список
+        self.browser.find_element(By.LINK_TEXT, "My lists").click()
+        self.wait_for(
+            lambda: self.browser.find_element(By.LINK_TEXT, "Click cows")
+        )
+        self.browser.find_element(By.LINK_TEXT, "Click cows").click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, second_list_url)
+        )
+
+        # Она выходит из системы. Опция "мои списки" исчезает
+        self.browser.find_element(By.LINK_TEXT, "Log out").click()
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_elements_by_link_text('My lists'),
+                [],
+            )
+        )
--- lists/urls.py
+++ lists/urls.py
@@ -7,6 +7,7 @@ from lists import views
 urlpatterns = [
     path("new", views.new_list, name="new_list"),
     path("<int:list_id>/", views.view_list, name="view_list"),
+    path("users/<str:email>/", views.my_lists, name="my_lists"),
 ]
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
