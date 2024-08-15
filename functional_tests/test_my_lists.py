6b5a45f636d8e03b3e92c61ee2d48804e2a1ba71
Anton Masyuk <pantera_box@mail.ru>
2024-08-15 16:38:32 +0500

    Добавлен скриншот на неполадке в исполнителе ФТ

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,11 +1,11 @@
-import selenium
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from selenium.webdriver.common.by import By
 
 from functional_tests.base import FunctionalTest
-from functional_tests.management.commands.create_session import \
-    create_pre_authenticated_session
+from functional_tests.management.commands.create_session import (
+    create_pre_authenticated_session,
+)
 from .server_tools import create_session_on_server
 
 User = get_user_model()
@@ -17,8 +17,8 @@ class MyListsTest(FunctionalTest):
             session_key = create_session_on_server(self.staging_server, email)
         else:
             session_key = create_pre_authenticated_session(email)
-        ## Установить cookie, которые нужны для первого посещения домена.
-        ## страницы 404 загружаются быстрее всего.
+        # Установить cookie, которые нужны для первого посещения домена.
+        # Страницы 404 загружаются быстрее всего.
         self.browser.get(self.live_server_url + "/404_no_such_url/")
         self.browser.add_cookie(
             dict(
@@ -58,9 +58,7 @@ class MyListsTest(FunctionalTest):
 
         # Под заголовком "мои списки" появляется ее новый список
         self.browser.find_element(By.LINK_TEXT, "My lists").click()
-        self.wait_for(
-            lambda: self.browser.find_element(By.LINK_TEXT, "Click cows")
-        )
+        self.wait_for(lambda: self.browser.find_element(By.LINK_TEXT, "Click cows"))
         self.browser.find_element(By.LINK_TEXT, "Click cows").click()
         self.wait_for(
             lambda: self.assertEqual(self.browser.current_url, second_list_url)
@@ -70,7 +68,7 @@ class MyListsTest(FunctionalTest):
         self.browser.find_element(By.LINK_TEXT, "Log out").click()
         self.wait_for(
             lambda: self.assertEqual(
-                self.browser.find_elements(By.LINK_TEXT, 'My lists'),
+                self.browser.find_elements(By.LINK_TEXT, "My lists"),
                 [],
             )
         )
