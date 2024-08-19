1d8ab7fbdd924f9ab2e07a7fdd6a16eef7b25dbb
Anton Masyuk <pantera_box@mail.ru>
2024-08-19 16:54:56 +0500

    Созданы страничные объекты для страниц списков, использовать в ФТ с обменом списками

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 from .base import FunctionalTest
+from .list_page import ListPage
+from .my_lists_page import MyListsPage
 
 
 def quit_if_possible(browser):
@@ -20,26 +22,49 @@ class SharingTest(FunctionalTest):
         self.addCleanup(lambda: quit_if_possible(edith_browser))
 
         # Ее друг Анцифер тоже зависает на сайте списков
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
         oni_browser = webdriver.Chrome(
-            options=chrome_options,
+            options=self._options,
         )
         self.addCleanup(lambda: quit_if_possible(oni_browser))
         self.browser = oni_browser
+        self.browser.get(self.live_server_url)
         self.create_pre_authenticated_session("oniciferous@example.com")
 
         # Эдит открывает домашнюю страницу и начинает новый поиск
+
         self.browser = edith_browser
         self.browser.get(self.live_server_url)
-        self.add_list_item("Get help")
+        list_page = ListPage(self).add_list_item("Get help")
 
         # Она замечает опцию "поделиться этим списком"
-        share_box = self.browser.find_element(
-            By.CSS_SELECTOR,
-            "input[name='share']",
-        )
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute("placeholder"),
             "your-friend@example.com",
         )
+
+        # Она делится своим списком
+        # Страница обновляется и сообщает, что теперь страница используется
+        # Совместно с Анцифером
+        list_page.share_list_with("oniciferous@example.com")
+
+        # Анцифер переходит на страницу списков в своем браузере
+        self.browser = oni_browser
+        MyListsPage(self).go_to_my_lists_page()
+
+        # Он видит список Эдит!
+        self.browser.find_element(By.LINK_TEXT, "Get help").click()
+
+        # На странице, которую Анцифер видит, говорится, что это список Эдит
+        self.wait_for(
+            list_page.get_list_owner(),
+            "edith@example.com",
+        )
+
+        # Он добавляет элемент в список
+        list_page.add_list_item("Hi Edith!")
+
+        # Когда Эдит обновляет страницу, она видит дополнение Анцифера
+        self.browser = edith_browser
+        self.browser.refresh()
+        list_page.wait_for_row_in_list_table("Hi Edith!", 2)
